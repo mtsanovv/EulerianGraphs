8935f0d555f45e54d26dc9dbb49432a1f3c6aa96
Marin Tsanov <btg.tsanov.marin@gmail.com>
2021-03-18 22:24:44 +0200

    Created the Eulerian cycle check

---
+++ js/GraphModel.js
@@ -0,0 +1,121 @@
+class GraphModel {
+    constructor(graphInput, sequenceInput, errorField) {
+        this.graphInput = graphInput;
+        this.sequenceInput = sequenceInput;
+        this.errorField = errorField;
+        this.G = [];
+    }
+    //verify the graph input
+    verifyInput() {
+        const edges = this.graphInput.split(/\n+/);
+        if(edges.length == 0 || (edges.length == 1 && edges[0].length == 0)) {
+            //a graph with no edges
+            return true;
+        }
+        for(const edge of edges) {
+            const vertices = edge.split(/\s+/);
+            if(vertices.length != 2) {
+                this.errorField.innerText = 'Invalid graph submitted: one of the edges contains more than two space-separated values. Please also check for any extra and/or trailing whitespaces.';
+                return false;
+            } else if(vertices.length == 1 && vertices[0].length == 0) {
+                this.errorField.innerText = 'Invalid graph submitted: one of the edges is empty.';
+                return false;
+            }
+            for(const vertex of vertices) {
+                if(!vertex.length) {
+                    this.errorField.innerText = 'Invalid graph submitted: one of the vertices in one of the edges is empty.';
+                    return false;
+                } else if(!vertex.length || !Number.isInteger(Number(vertex))) {
+                    this.errorField.innerText = 'Invalid graph submitted: one of the vertices in one of the edges is not an integer. Please also check for any extra and/or trailing whitespaces.';
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    //convert the graph input into an actual graph
+    graphify() {
+        const edges = this.graphInput.split(/\n+/);
+        for(const edge of edges) {
+            const vertices = edge.split(/\s+/);
+            if(vertices.length != 2) {
+                continue;
+            }
+            if(!Array.isArray(this.G[Number(vertices[0])])) {
+                this.G[Number(vertices[0])] = [];
+            }
+            this.G[Number(vertices[0])].push(Number(vertices[1]));
+
+            if(!Array.isArray(this.G[Number(vertices[1])])) {
+                this.G[Number(vertices[1])] = [];
+            }
+            this.G[Number(vertices[1])].push(Number(vertices[0]));
+        }
+    }
+    //implement the depth-first search to traverse the graph
+    dfs(root, checked) {
+        //mark the root as checked
+        checked[root] = true;
+
+        //do the same for all the vertices adjacent to this vertex
+        for(let i = 0; i < this.G[root].length; i++) {
+            if(!checked[this.G[root][i]]) {
+                this.dfs(this.G[root][i], checked);
+            }
+        }
+    }
+    //method to check if all non-zero degree vertices are connected
+    verticesConnected() {
+        if(!this.G.length) {
+            return true; //a graph with no edges is also Eulerian
+        }
+        let checked = [];
+        for(let i = 0; i < this.G.length; i++) {
+            if(Array.isArray(this.G[i])) {
+                checked[i] = false;
+            }
+        }
+        let root;
+        for(let i = 0; i < this.G.length; i++) {
+            if(!Array.isArray(this.G[i])) {
+                continue;
+            }
+            if(this.G[i].length) {
+                root = i;
+                break;
+            }
+        }
+
+        //root can never be undefined because if we have edges, we have non-zero degree vertices
+        this.dfs(root, checked);
+        for(let i = 0; i < this.G.length; i++) {
+            if(!Array.isArray(this.G[i])) {
+                continue;
+            }
+            if(this.G[i].length && !checked[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    //check if there is at least one Eulerian cycle in the graph
+    hasEulerianCycle() {
+        if(!this.verticesConnected()) {
+            return false;
+        }
+
+        //count of vertices with odd degrees
+        let odd = 0;
+        for(let i = 0; i < this.G.length; i++) {
+            if(Array.isArray(this.G[i]) && this.G[i].length % 2 != 0) {
+                odd++;
+            }
+        }
+
+        if(!odd) {
+            return true; //if no odd vertices, we can be sure that there is at least one Eulerian cycle
+        }
+        return false; //semi-eulerian and non-eulerian mean there's no eulerian cycle
+    }
+
+}
--- js/index.js
+++ js/index.js
@@ -9,9 +9,17 @@ function createGraph() {
     const graphInput = document.getElementById('graphInput').value;
     const sequenceInput = document.getElementById('sequenceInput').value;
     const errorField = document.getElementById('errorField');
+    const outputField = document.getElementById('output');
     errorField.innerText = '';
-    let g = new G(graphInput, sequenceInput, errorField);
-    if(g.verifyInput) {
-        g.graphify();
+    outputField.innerHTML = '';
+    let G = new GraphModel(graphInput, sequenceInput, errorField);
+    if(G.verifyInput()) {
+        G.graphify();
+        const hasEulerianCycle = G.hasEulerianCycle()
+        if(hasEulerianCycle) {
+            outputField.innerHTML += "<ul><li style='color: rgb(6, 116, 3);'>The graph has an Eulerian cycle.</li></ul>";
+        } else {
+            outputField.innerHTML += "<ul><li style='color: rgb(201, 0, 0);'>The graph does not have an Eulerian cycle.</li></ul>";
+        }
     }
 }
